c9b912f9cc6b4c15d6e9222466de1fa4a95af2c6
lucasDemaria60 <131877186+lucasDemaria60@users.noreply.github.com>
2023-09-27 01:18:43 -0300

    ?

--- interfaces/base de datos programacion.py
+++ interfaces/base de datos programacion.py
@@ -3,84 +3,40 @@ from tkinter import ttk
 from tkinter import messagebox
 import mysql.connector
 
+# Conexión a la base de datos MySQL
 conexion = mysql.connector.connect(host="localhost", user="root", password="", database="ESCUELA")
 
+# Función para cargar y mostrar información en el Treeview
 def cargar_datos():
-    tree.delete(*tree.get_children())  
+    tree.delete(*tree.get_children())  # Borrar datos existentes en el Treeview
     cursor = conexion.cursor()
-    cursor.execute("select alumnos.nombre, alumnos.apellido, alumnos.dni, Carrera.nombre, estado_alumno.nombre from alumnos join carrera on alumnos.codcarrera = carrera.codcarrera join estado_alumno on estado_alumno.cod_estado_alumno = alumnos.cod_estado_alumno where alumnos.cod_estado_alumno !=2")
+    cursor.execute("select alumnos.nombre, alumnos.apellido, alumnos.dni, Carrera.nombre, estado_alumno.nombre from alumnos join carrera on alumnos.codcarrera = carrera.codcarrera join estado_alumno on estado_alumno.cod_estado_alumno = alumnos.cod_estado_alumno")
     for row in cursor.fetchall():
         tree.insert("", "end", values=row)
 
-
-def modificar_datos():
-    dicc = tree.item(tree.selection())
-    cambiar = dicc ['values'][2]
-    nombre = nombre_entry.get()
-    apellido = apellido_entry.get()
-    dni = dni_entry.get()
-    nombre_carrera = carrera_combobox.get()
-    estado_alumno = 1
-    
-    if nombre and apellido and dni and nombre_carrera:
-        if(( len(dni) > 6 ) and ( len(dni) < 9)) and dni.isnumeric():
-            
-            carreras = cargar_carreras()
-            carrera_id = None
-            for carrera in carreras:
-                if carrera[1] == nombre_carrera:
-                    carrera_id = carrera[0]
-                    break
-
-            cursor = conexion.cursor()
-
-            cursor.execute("""update alumnos set
-                               nombre = %s,
-                               apellido = %s,
-                               dni = %s,
-                               id_carrera = %s,
-                               id_estado_alumno = %s
-                               where dni = %s""", (nombre, apellido, dni, carrera_id, estado_alumno, cambiar))
-            conexion.commit()
-            cargar_datos()  
-            nombre_entry.delete(0, tk.END)
-            apellido_entry.delete(0, tk.END)
-            dni_entry.delete(0, tk.END)
-            carrera_combobox.set("")  
-        else:
-            mostrar_alerta("DNI no valido")
-    else:
-        mostrar_alerta("Los campos son obligatorios. Debe completarlos.")
-
-    
-    estado_alumno = 1  
-    cursor = conexion.cursor()
-    
-    conexion.commit()
-    cargar_datos()  
-    nombre_entry.delete(0, tk.END)
-    apellido_entry.delete(0, tk.END)
-    dni_entry.delete(0, tk.END)
-    carrera_combobox.set("")  
+# Función para obtener las carreras desde la base de datos y cargarlas en el ComboBox
 def cargar_carreras():
     cursor = conexion.cursor()
     cursor.execute("SELECT codCARRERA, NOMBRE FROM Carrera ORDER BY NOMBRE")
     carreras = cursor.fetchall()
     carrera_combobox['values'] = [row[1] for row in carreras]
-    return carreras  
+    return carreras  # Devolver también la lista de carreras con sus IDs
+
+# Función para mostrar una ventana de alerta
 def mostrar_alerta(mensaje):
     messagebox.showwarning("Alerta", mensaje)
 
+# Función para guardar un nuevo registro de alumno
 def guardar_alumno():
     nombre = nombre_entry.get().upper()
     apellido = apellido_entry.get().upper()
     dni = dni_entry.get()
     if(( len(dni) > 6 ) and ( len(dni) < 9)) and dni.isnumeric():
         carrera_nombre = carrera_combobox.get()
-        estado_alumno = 1 
+        estado_alumno = 1  # Valor predeterminado para IDESTADOALUMNO
 
         if nombre and apellido and dni and carrera_nombre:
-           
+            # Obtener el ID de la carrera seleccionada
             carreras = cargar_carreras()
             carrera_id = None
             for carrera in carreras:
@@ -89,13 +45,15 @@ def guardar_alumno():
                     break
 
             cursor = conexion.cursor()
+            # Insertar un nuevo registro en la tabla Alumnos con el ID de carrera y el valor predeterminado para IDESTADOALUMNO
             cursor.execute("INSERT INTO Alumnos (NOMBRE, APELLIDO, DNI, codCARRERA, cod_ESTADO_ALUMNO) VALUES (%s, %s, %s, %s, %s)", (nombre, apellido, dni, carrera_id, estado_alumno))
             conexion.commit()
-            cargar_datos()  
+            cargar_datos()  # Actualizar la vista
+            # Limpiar los campos después de insertar
             nombre_entry.delete(0, tk.END)
             apellido_entry.delete(0, tk.END)
             dni_entry.delete(0, tk.END)
-            carrera_combobox.set("")  
+            carrera_combobox.set("")  # Limpiar la selección del ComboBox
         else:
             mostrar_alerta("Los campos son obligatorios. Debe completarlos.")
     else:
@@ -104,15 +62,19 @@ def guardar_alumno():
 """
 Añadir a la función de cargar_datos  el WHERE para que liste sólo los alumnos que están "REGULARES" es decir tengan id_estado_alumno = 1, para que no nos liste a los alumnos que están libres.
 """
+# Crear ventana
 root = tk.Tk()
 root.title("Consulta de Alumnos")
 
+# Crear un frame con un borde visible para el formulario de inscripción
 formulario_frame = tk.Frame(root, bd=2, relief=tk.SOLID)
 formulario_frame.pack(padx=10, pady=10)
 
+# Título del formulario
 titulo_label = tk.Label(formulario_frame, text="Formulario Inscripción", font=("Helvetica", 14))
 titulo_label.grid(row=0, column=0, columnspan=2, pady=10)
 
+# Campos de entrada para nombre, apellido y DNI con el mismo ancho que el ComboBox
 nombre_label = tk.Label(formulario_frame, text="Nombre:")
 nombre_label.grid(row=1, column=0)
 nombre_entry = tk.Entry(formulario_frame)
@@ -128,30 +90,35 @@ dni_label.grid(row=3, column=0)
 dni_entry = tk.Entry(formulario_frame)
 dni_entry.grid(row=3, column=1, padx=5, pady=5, ipadx=5, ipady=5, sticky="ew")
 
+# Combo box para la carrera
 carrera_label = tk.Label(formulario_frame, text="Carrera:")
 carrera_label.grid(row=4, column=0)
-carrera_combobox = ttk.Combobox(formulario_frame,  state="readonly")
+carrera_combobox = ttk.Combobox(formulario_frame,  state="readonly")# Configurar el ComboBox como de solo lectura
 carrera_combobox.grid(row=4, column=1, padx=5, pady=5, ipadx=5, ipady=5, sticky="ew")
 
+# Cargar las carreras al inicio de la aplicación y obtener la lista de carreras con sus IDs
 carreras = cargar_carreras()
 
+# Botón para guardar un nuevo registro de alumno
 guardar_button = tk.Button(formulario_frame, text="Guardar", command=guardar_alumno)
 guardar_button.grid(row=5, columnspan=2, pady=10, sticky="ew")
 
+# Crear Treeview para mostrar la información
 tree = ttk.Treeview(root, columns=("Nombre", "Apellido","DNI", "Carrera", "Estado del alumno"))
 tree.heading("#1", text="Nombre")
 tree.heading("#2", text="Apellido")
 tree.heading("#3", text="DNI")
 tree.heading("#4", text="Carrera")
 tree.heading("#5", text="Estado Alumno")
-tree.column("#0", width=0, stretch=tk.NO)  
+tree.column("#0", width=0, stretch=tk.NO)  # Ocultar la columna #0 que habitualmente muestra las primary key de los objetos
 tree.pack(padx=10, pady=10)
 
+# Botón para cargar datos
 cargar_button = tk.Button(root, text="Cargar Datos", command=cargar_datos)
 cargar_button.pack(pady=5)
 
-
+# Ejecutar la aplicación
 root.mainloop()
 
-
+# Cerrar la conexión a la base de datos al cerrar la aplicación
 conexion.close()
